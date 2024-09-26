edcecbb2ac65481b64d525dbfcd4b1a7aef2f5c4
Brian Benz <bbenz@microsoft.com>
2024-09-26 00:28:02 -0700

    Add models to ollama separately

--- src/main/k8s/deplytok8s.sh
+++ src/main/k8s/deplytok8s.sh
@@ -8,3 +8,8 @@ kubectl apply -f ollama-webui-deployment.yaml
 kubectl apply -f qdrant-deployment.yaml
 kubectl apply -f demo-application-deployment.yaml
 kubectl apply -f ingress.yaml
+
+# Add models into Ollama
+kubectl get pods -l app=ollama
+OLLAMA_POD=$(kubectl get pods -l app=ollama -o jsonpath="{.items[0].metadata.name}")
+kubectl exec -it $OLLAMA_POD -- /bin/bash /install-ollama-models.sh
